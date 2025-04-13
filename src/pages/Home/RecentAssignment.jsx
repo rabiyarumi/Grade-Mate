13ff99323dbf7f81410a015d83d0a070dc8d50c1
rabiyarumi <rabiyaarumi@gmail.com>
2025-04-14 00:11:25 +0600

    show recent assignment

--- src/pages/Home/RecentAssignment.jsx
+++ src/pages/Home/RecentAssignment.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Assignment from '../Assignments/Assignment';
+import { section } from 'framer-motion/client';
 
 const RecentAssignment = () => {
       const [assignments, setAssignments] = useState([])
@@ -19,9 +21,16 @@ const RecentAssignment = () => {
 
       console.log(assignments)
     return (
-        <div>
+        <section className="bg-base-100 py-16 md:w-[85%] mx-auto">
+          <div className='container mx-auto '>
+                    <h2 className="text-3xl font-bold mb-8 text-center">Checkout Latest <span className="text-accent">Assignments</span> </h2>
+
             Recent Assignment {assignments.length}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+              {assignments.slice().reverse().slice(0,6).map(assignment => <Assignment key={assignment?._id} assignment={assignment}/>)}
+            </div>
         </div>
+        </section>
     );
 };
 
