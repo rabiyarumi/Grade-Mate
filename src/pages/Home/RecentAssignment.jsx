c8968d0865d24cd2fb10d0b592bf4c134c241756
rabiyarumi <rabiyaarumi@gmail.com>
2025-04-13 23:51:53 +0600

    fetch recent assignment in home page

--- src/pages/Home/RecentAssignment.jsx
+++ src/pages/Home/RecentAssignment.jsx
@@ -2,23 +2,25 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const RecentAssignment = () => {
-
-    const [assignments, setAssignments] = useState([])
-
-    useEffect(() => {
-
-        const fetchAssignments = async () => {
-          const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/assignments`)
-          setAssignments(data)
-          }
-
-        fetchAssignments()
-      }, [])
+      const [assignments, setAssignments] = useState([])
+      const [filter, setFilter] = useState("")
+      const [search, setSearch] = useState("")
+      console.log(assignments)
+  
+      useEffect(() => {
+  
+          const fetchAllAssignments = async () => {
+              const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/assignments?filter=${filter}&search=${search}`)
+              setAssignments(data)
+            }
+  
+          fetchAllAssignments()
+        }, [filter, search])
 
       console.log(assignments)
     return (
         <div>
-            {/* Recent Assignment {assignments.length} */}
+            Recent Assignment {assignments.length}
         </div>
     );
 };
